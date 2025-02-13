dead7c79870c4043995819bd839e2fcbc134d872
codeveno <samsonochieng476@gmail.com>
2025-02-13 13:57:22 +0300

    string search algorothms

---
+++ string/string manipulation.py
@@ -0,0 +1,156 @@
+#string manipulation in python
+#Strings are sequences of characters.
+#Strings are immutable.
+#Strings are enclosed in single quotes or double quotes.
+#Strings can be concatenated using the + operator.
+#Strings can be repeated using the * operator.
+#Strings can be accessed using the index operator [].   
+#Strings can be sliced using the slice operator [:].
+#Strings can be formatted using the format() method.
+#Strings can be formatted using f-strings.
+#Strings can be formatted using the % operator.
+#Strings can be formatted using the format() function.
+#Strings can be formatted using the Template class.
+
+#example
+#string concatenation
+string1 = 'Hello'
+string2 = 'World'
+string3 = string1 + ' ' + string2
+print(string3)
+
+#string repetition
+string4 = 'Hello ' * 3
+print(string4)
+
+#string access
+string5 = 'Hello World'
+print(string5[0])
+print(string5[1])
+print(string5[2])
+print(string5[3])
+print(string5[4])
+print(string5[5])
+print(string5[6])
+print(string5[7])
+print(string5[8])
+print(string5[9])
+print(string5[10])
+
+#string slicing
+string6 = 'Hello World'
+print(string6[0:5])
+print(string6[6:11])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string/string searching algorothms.py
@@ -0,0 +1,25 @@
+#string searching algorithms
+#String searching algorithms are algorithms that search for a substring in a string.
+#There are several string searching algorithms, including the brute force algorithm, the Boyer-Moore algorithm, and the Knuth-Morris-Pratt algorithm.
+#The brute force algorithm is the simplest string searching algorithm.
+#The brute force algorithm compares the substring with the string character by character.
+#The brute force algorithm has a time complexity of O(n*m), where n is the length of the string and m is the length of the
+#substring.
+#The Boyer-Moore algorithm is a more efficient string searching
+#algorithm than the brute force algorithm.
+#The Boyer-Moore algorithm uses a heuristic to skip characters in the string that do not match the substring.
+#The Boyer-Moore algorithm has a time complexity of O(n+m), where n is the length of the string and m is the length of the substring.
+#The Knuth-Morris-Pratt algorithm is another efficient string searching algorithm.
+#The Knuth-Morris-Pratt algorithm uses a prefix function to skip characters in the string that do not match the substring.
+#The Knuth-Morris-Pratt algorithm has a time complexity of O(n+m), where n is the length of the string and m is the length of the substring.
+#The Rabin-Karp algorithm is another string searching algorithm that uses hashing to search for a substring in a string.
+#The Rabin-Karp algorithm has a time complexity of O(n+m), where n is the length of the string and m is the length of the substring.
+#The Aho-Corasick algorithm is another string searching algorithm that uses a trie data structure to search for multiple substrings in a string.
+#The Aho-Corasick algorithm has a time complexity of O(n+m), where n is the length of the string and m is the length of the substring.
+#The Z algorithm is another string searching algorithm that uses a Z array to search for a substring in a string.
+#The Z algorithm has a time complexity of O(n+m), where n is the length of the string and m is the length of the substring.
+#The string searching algorithms are used in text editors, search engines, and other applications that require searching for a substring in a string.
+#The string searching algorithms are used in text editors, search engines, and other applications that require searching for a substring in a string.
+
+#example
+#string searching algorithms
